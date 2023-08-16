1647f76ad1a8192210a744a3b6c91dc74159ecf3
Guy <guykibby@gmail.com>
2023-08-17 07:31:45 +1000

    delete comment

--- App.js
+++ App.js
@@ -90,7 +90,6 @@ export default function App() {
             renderItem={TodoItem}
             renderHiddenItem={(data, rowMap) =>
               TodoItemButtons(data, rowMap, (rowMap, deleteThis) => {
-                // TIP: deletes a task/row
                 closeRow(rowMap, deleteThis);
                 const newData = [...listData];
                 const i = newData.findIndex((rowItem) => rowItem.key === 0);
