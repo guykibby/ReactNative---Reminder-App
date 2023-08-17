243d885351b65b64a743ca69a845078e6855c964
Guy <guykibby@gmail.com>
2023-08-17 20:32:05 +1000

    refactor add new task

--- App.js
+++ App.js
@@ -7,25 +7,10 @@ import TodoItem from "./components/TodoItem";
 import TodoItemButtons from "./components/TodoItemButtons";
 import AddTodo from "./components/AddTodo";
 import { getStorage, updateStorage } from "./api/localStorage";
-import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function App() {
   const [listData, setListData] = useState([]);
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [dateTimePickerMode, setDateTimePickerMode] = useState("date");
-  const [taskName, setTaskName] = useState("");
-  const [selectedDate, setSelectedDate] = useState(new Date());
-
-  const addTask = (dateTime) => {
-    const newData = [...listData];
-    newData.push({
-      name: taskName,
-      timestamp: dateTime,
-      key: new Date().getTime().toString(),
-    });
-    setListData(newData);
-    setDateTimePickerMode("date");
-  };
+  console.log("listData: " + listData);
 
   const datePickerMode = (currentMode) => {
     setShowDatePicker(true);
@@ -71,14 +56,7 @@ export default function App() {
         >
           Reminders
         </Text>
-        <AddTodo
-          add={(name) => {
-            // TIP: handles add button being pressed
-            setTaskName(name);
-            setSelectedDate(new Date());
-            setShowDatePicker(true);
-          }}
-        />
+        <AddTodo setListData={setListData} />
         <View
           style={{
             backgroundColor: "white",
@@ -107,35 +85,6 @@ export default function App() {
           />
         </View>
       </View>
-
-      {showDatePicker ? (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={selectedDate}
-          mode={dateTimePickerMode}
-          onChange={(event, selectedValue) => {
-            setShowDatePicker(false);
-
-            if (selectedValue) {
-              const currentSelectedDate = new Date(selectedValue);
-              if (dateTimePickerMode === "date") {
-                setSelectedDate(currentSelectedDate);
-                setDateTimePickerMode("time");
-                setShowDatePicker(true);
-              } else if (dateTimePickerMode === "time") {
-                const newDate = new Date(selectedDate);
-                newDate.setHours(
-                  currentSelectedDate.getHours(),
-                  currentSelectedDate.getMinutes()
-                );
-                addTask(newDate);
-              }
-            } else {
-              setDateTimePickerMode("date");
-            }
-          }}
-        />
-      ) : null}
     </View>
   );
 }
