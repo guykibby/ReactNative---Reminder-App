5cabdda82bc1d4da009727b7af2cdd3b168f9710
Guy <guykibby@gmail.com>
2023-08-19 06:59:22 +1000

    Notifications setup

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import * as Notifications from "expo-notifications";
+import {
+  registerForNotifications,
+  setNotificationFor,
+} from "./api/notification";
 import TaskPanel from "./components/TaskPanel";
 import AddTask from "./components/AddTask";
 import { getStorage, updateStorage } from "./api/localStorage";
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
   const [taskList, setTaskList] = useState([]);
 
-  let isMounted = false;
-
   useEffect(() => {
-    isMounted = true;
-    console.warn("app mounted");
+    registerForNotifications().then((token) => setExpoPushToken(token));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response.notification.request.content.data);
+      });
+
     return () => {
-      isMounted = false;
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
     };
   }, []);
 
@@ -23,6 +43,37 @@ export default function App() {
       <AddTask setTaskList={setTaskList} />
       <View style={styles.panel}>
         <TaskPanel taskList={taskList} setTaskList={setTaskList} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <Text>Your expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Text>
+            Title: {notification && notification.request.content.title}{" "}
+          </Text>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>
+            Data:{" "}
+            {notification && JSON.stringify(notification.request.content.data)}
+          </Text>
+        </View>
+        <Button
+          title="Press to schedule a notification"
+          onPress={async () => {
+            const trigger = new Date();
+            trigger.setSeconds(trigger.getSeconds() + 2);
+            const newTask = {
+              name: "Test",
+              timestamp: trigger,
+            };
+            await setNotificationFor(newTask);
+          }}
+        />
       </View>
     </View>
   );
