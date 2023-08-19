5fc789792723030a480ddc32b8db9dceb57a6f2a
Guy <guykibby@gmail.com>
2023-08-19 16:27:41 +1000

    Refactor taskPanel and addTask

--- App.js
+++ App.js
@@ -1,42 +1,12 @@
-import { useState, useEffect, useRef } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { useEffect } from "react";
 import { registerForNotifications } from "./api/notification";
 import TaskPanel from "./components/TaskPanel";
-import AddTask from "./components/AddTask";
 import { getStorage, updateStorage } from "./api/localStorage";
 
 export default function App() {
-  const [taskList, setTaskList] = useState([]);
-
   useEffect(() => {
     registerForNotifications();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Reminders</Text>
-      <AddTask setTaskList={setTaskList} />
-      <View style={styles.panel}>
-        <TaskPanel taskList={taskList} setTaskList={setTaskList} />
-      </View>
-    </View>
-  );
+  return <TaskPanel />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-around",
-    marginTop: 50,
-    height: "100%",
-  },
-  heading: {
-    textAlign: "center",
-    fontSize: 20,
-  },
-  panel: {
-    backgroundColor: "white",
-    flex: 1,
-  },
-});
