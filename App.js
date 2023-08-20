d80944a87eea20facc84596cd4e6181e8aecfc76
Guy <guykibby@gmail.com>
2023-08-20 10:06:57 +1000

    Refctor - taskpanel, addTask

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { useEffect } from "react";
 import { registerForNotifications } from "./api/notification";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import TaskPanel from "./components/TaskPanel";
 import { getStorage, updateStorage } from "./api/localStorage";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   useEffect(() => {
     registerForNotifications();
   }, []);
 
-  return <TaskPanel />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="TaskPanel"
+          component={TaskPanel}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
