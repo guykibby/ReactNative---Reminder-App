1e0fc4b5f064021fec9d3d904c2c2825d2b575ed
Guy <guykibby@gmail.com>
2023-08-21 10:43:57 +1000

    Simplify Authentication check

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -2,47 +2,12 @@ import { View, Text, Alert, Pressable } from "react-native";
 import * as LocalAuthentication from "expo-local-authentication";
 
 const LoginScreen = ({ navigation }) => {
-  const supportsAndEnrolledInBiometrics = async () => {
-    const compatible = await LocalAuthentication.hasHardwareAsync();
-    if (!compatible) {
-      Alert.alert(
-        "Device Incompatibility",
-        "Your device does not support biometric authentication."
-      );
-      return false;
-    }
-
-    const bioEnabled = await LocalAuthentication.isEnrolledAsync();
-    if (!bioEnabled) {
-      Alert.alert(
-        "Biometrics Not Setup",
-        "You have not set up biometric authentication on this device."
-      );
-      return false;
-    }
-
-    return true;
-  };
-
-  const authenticate = async () => {
+  const handlePress = async () => {
     const result = await LocalAuthentication.authenticateAsync();
-
     if (result.success) {
       navigation.navigate("HomePage");
     } else {
       Alert.alert("Authentication failed. Please try again.");
-    }
-  };
-
-  const handlePress = async () => {
-    const isSupported = await supportsAndEnrolledInBiometrics();
-    console.log(isSupported);
-    if (isSupported) {
-      await authenticate();
-    } else {
-      Alert.alert(
-        "Youre device has no screen lock protection, please set one up."
-      );
     }
   };
 
