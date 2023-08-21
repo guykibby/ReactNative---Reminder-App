b7f971059230973d1c7e7886785c1e00341046a4
Guy <guykibby@gmail.com>
2023-08-21 11:04:34 +1000

    Add authentication fail alerts

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -4,10 +4,15 @@ import * as LocalAuthentication from "expo-local-authentication";
 const LoginScreen = ({ navigation }) => {
   const handlePress = async () => {
     const result = await LocalAuthentication.authenticateAsync();
+    console.log(result);
     if (result.success) {
       navigation.navigate("HomePage");
+    } else if (result.error === "not_enrolled") {
+      Alert.alert(
+        "Please enable device security! For your privacy we rquire lock screen security to use this app."
+      );
     } else {
-      Alert.alert("Authentication failed. Please try again.");
+      Alert.alert("Authentication failed.");
     }
   };
 
